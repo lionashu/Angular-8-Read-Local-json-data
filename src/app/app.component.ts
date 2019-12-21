ff77e7629b5c667acdeefaefc2403b539c4575e5
lionashu <ashishmipra@gmail.com>
2019-12-21 18:32:33 +0530

    Angular 8 read local json data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import dummydata from './_files/dummydata.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'readlocal-json';
+  title = 'Angular 8 Read Local JSON Data';
+  public nameData: {name: string, surname: string}[] = dummydata;
 }
